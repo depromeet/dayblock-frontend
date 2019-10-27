d4c4c507c220498ea1556f158220d53f3dc03e18
eunbi <silver23rain@gmail.com>
2019-10-27 11:30:50 +0900

    [Setting] sass setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Home } from './componenets/pages';
+import { Home, Test } from './componenets/pages';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Route exact path="/" component={Home}/>
-                <Route path="/test" component={Home}/>
+                <Route path="/test" component={Test}/>
             </div>
         );
     }
---
+++ src/componenets/pages/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../resources/sass/Test.scss"
+
+const Test = () => {
+    return (
+        <div class="test-parent">
+            <h1>sass test</h1>
+        </div>
+    );
+};
+
+export default Test;
