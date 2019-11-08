693fc987a0029e303ac80d2a41bdb996f385a7f4
girin-dev <kw8799@gmail.com>
2019-11-08 21:55:15 +0900

    [Layout] Header, Nav 추가

---
+++ src/componenets/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './common.css';
+import './header.css';
+
+
+const Header = () => {
+    return (
+      <header class="header">
+        <div class="header-logo"></div>
+      </header>  
+    );
+};
+
+export default Header;
---
+++ src/componenets/layout/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './common.css';
+import './nav.css';
+
+
+const Nav = () => {
+    return (
+      <Nav class="Nav">
+        <div class="Nav-logo"></div>
+      </Nav>  
+    );
+};
+
+export default Nav;
